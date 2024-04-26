7f3650c647b3351b87f354c8a87c029e61a40567
AnzenKodo <AnzenKodo@altmails.com>
2024-04-26 18:45:13 +0530

    Edited examples.c

--- examples.c
+++ examples.c
@@ -1,5 +1,4 @@
 #include "arena.h"
-#include "hash.h"
 #include "str.h"
 #include "base.h"
 #include "args.h"
@@ -36,25 +35,6 @@ void arena_test(void) {
     arena_free(&arena);
 }
 
-void ht_test(void) {
-    assert(hash_stb6432shift("Hello", 1) == 7871771224773941061);
-    assert(hash_djbx33a("Hello") == 140207504);
-    assert(hash_djb2("Hello") == 210676686969);
-    assert(hash_sdbm("Hello") == 5142962386210502930);
-    assert(hash_2lose("Hello") == 500);
-    assert(hash_adler32("Hello") == 93061621);
-    assert(hash_crc32("Hello") == (unsigned int) -137262718);
-    assert(hash_xor8("Hello") == 12);
-
-    unsigned int num_mix = hash_int_mix(420, 12, 69);
-    assert(num_mix == 1501094981);
-    assert(hash_int_jenkins(num_mix) == 317653115);
-    assert(hash_int_wang32shift(num_mix) == 1265322998);
-    assert(hash_int_wang32shiftmult(num_mix) == 608512410);
-    assert(hash_int_wang6432shift(num_mix) == 981695088);
-    assert(hash_int_wang64shift(num_mix) == 5360857491066763507);
-}
-
 void args_test(int argc, char **argv) {
     args_init(argc, argv);
     args_get("--name", "-n", "Name of person");
@@ -72,6 +52,5 @@ void args_test(int argc, char **argv) {
 
 int main(int argc, char *argv[]) {
     arena_test();
-    ht_test();
     args_test(argc, argv);
 }
